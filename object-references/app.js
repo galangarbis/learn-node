c69437ddd96557206d3a0bac913d0153a465a694
galangarbis <galangarbis@gmail.com>
2017-10-03 13:26:36 +0700

    Object References

---
+++ object-references/app.js
@@ -0,0 +1,13 @@
+//deklarasi Object
+var myObject = {
+    name : "M Galang Arbi S",
+    age  : 21,
+};
+
+//Copy myObject to Var obj2
+var obj2 = myObject;
+//Obj2 age 19 Will Change Age from 21 to 19
+obj2.age = 19;
+
+//Show console of myObject
+console.log(myObject);
