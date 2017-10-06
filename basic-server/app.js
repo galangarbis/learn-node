7e259e07af066c768c9e75cc9c65dcbd8a559adf
galangarbis <galangarbis@gmail.com>
2017-10-06 23:53:26 +0700

    Basic Server

---
+++ basic-server/app.js
@@ -0,0 +1,12 @@
+//Import Modul HTTP NodeJS
+var http = require("http");
+//Create Server
+http.createServer(function (req, res) {
+    console.log(req.url);
+    res.writeHead(200, {"Content-Type" : "text/plain"});
+    res.write("Hello from Node Js Server");
+    res.write("You req "+req.url);
+    res.end();
+}).listen(3133); //<<=== Port listen 0n 3133
+
+console.log("Server is Running on 3133");
