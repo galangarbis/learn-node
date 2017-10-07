fbb7a3a5b8f01d8b9fa4a1eb469b3e9a3929253d
galangarbis <galangarbis@gmail.com>
2017-10-07 09:03:36 +0700

    simple file server part 2

--- simple-file-server/app.js
+++ simple-file-server/app.js
@@ -4,10 +4,27 @@ var http = require("http");
 var fs = require('fs');
 //Create Server
 http.createServer(function (req, res) {
+    //setting code for response
+    var resCode = 0;
+    var file = "";
+    //logic url
+    if (req.url == "/"){
+        //index page
+        resCode = 200;
+        file = "index.html";
+    } else if(req.url == "/contact"){
+        //contact page
+        resCode = 200;
+        file = "contact.html";
+    } else {
+        //404 Not Found page
+        resCode = 404;
+        file = "404.html";
+    }
     //Change from text/plain to text/html
-    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.writeHead(resCode, {"Content-Type" : "text/html"});
     //Call index.html
-    fs.createReadStream('./template/index.html').pipe(res);
+    fs.createReadStream('./template/'+file).pipe(res);
 }).listen(3133); //<<=== Port listen 0n 3133
 
 console.log("Server is Running on 3133");
