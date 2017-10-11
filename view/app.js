8a2e1ac97b75a0de53772b5ab1635cb198ec740f
galangarbis <galangarbis@gmail.com>
2017-10-11 14:27:34 +0700

    Render View Using Swig

--- view/app.js
+++ view/app.js
@@ -1,16 +1,17 @@
 var http = require("http");
 var url = require('url');
 var router = require('routes')();
+var view = require('swig');
 
 router.addRoute('/',function (req,res) {
-    res.writeHead(200, {"Content-Type" : "text/plain"});
-    res.end("Index Page");
+    var html = view.compileFile('./template/index.html')();
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
 });
 
 http.createServer(function (req,res) {
-
     var path = url.parse(req.url).pathname;
-    var match = routes.match(path);
+    var match = router.match(path);
     if (match){
         match.fn(req, res);
     } else {
@@ -18,6 +19,6 @@ http.createServer(function (req,res) {
         res.end("Page Not Found!");
     }
 
-}.listen(9090));
+}).listen(9090);
 
 console.log("Server is Running");
