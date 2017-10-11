1be215a86f7c5a162719c7fd10d99a319443473c
galangarbis <galangarbis@gmail.com>
2017-10-11 14:08:44 +0700

    View in node

---
+++ view/app.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var url = require('url');
+var router = require('routes')();
+
+router.addRoute('/',function (req,res) {
+    res.writeHead(200, {"Content-Type" : "text/plain"});
+    res.end("Index Page");
+});
+
+http.createServer(function (req,res) {
+
+    var path = url.parse(req.url).pathname;
+    var match = routes.match(path);
+    if (match){
+        match.fn(req, res);
+    } else {
+        res.writeHead(404, {"Content-Type" : "text/plain"});
+        res.end("Page Not Found!");
+    }
+
+}.listen(9090));
+
+console.log("Server is Running");
