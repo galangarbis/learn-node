c17cc1a4409a68c36cafd95e65de32f5953198b7
galangarbis <galangarbis@gmail.com>
2017-10-12 15:23:53 +0700

    Adding Inheritance in View Template Part 1

---
+++ view-inheritance/app.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var url = require('url');
+var router = require('routes')();
+var view = require('swig');
+
+router.addRoute('/',function (req,res) {
+    var html = view.compileFile('./template/index.html')({
+        title : "Index Page From Galang AS"
+    });
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
+});
+
+router.addRoute('/contact',function (req,res) {
+    var html = view.compileFile('./template/contact.html')();
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
+});
+
+http.createServer(function (req,res) {
+    var path = url.parse(req.url).pathname;
+    var match = router.match(path);
+    if (match){
+        match.fn(req, res);
+    } else {
+        res.writeHead(404, {"Content-Type" : "text/plain"});
+        res.end("Page Not Found!");
+    }
+
+}).listen(9090);
+
+console.log("Server is Running");
